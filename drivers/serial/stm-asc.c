a934349dc158505e920229bb673948923c89494c
Sean McGoogan <Sean.McGoogan@st.com>
2008-08-29 18:00:38 +0100

    [ASC] Bug-fix and refactoring of the (STM) ASC serial driver
    This update is a refactoring of the drivers/serial/stm-asc.c ASC driver
    file for the UART used by STMicroelectronics. This was primarily to
    address some confusion/misdirection over the accuracy of the comments,
    which had been reported a few times.  As a result, the comments are
    now correct, and there is a comment for each function, which should
    significantly address the maintainability of the file.
    
    As a result of the tidying up, some functions have been deleted, and
    (in some cases) recreated in a different guise, further improving the
    readability and maintainability of the driver.
    
    Code has been commented out, removing the support for the anachronistically
    slower baud rates (below 9600). If required, it can be re-enabled!
    
    Finally, a bug was fixed which had previously resulted in baud rates
    slower than 19200 being mis-configured with the wrong baud rate divisor.
    This has now been fixed, and all supported baud rates have been tested
    (on a mb628 board).
    
    The baud rates now supported are: 9600, 19200, 38400, 57600, 115200.
    
    Signed-off-by: Sean McGoogan <Sean.McGoogan@st.com>

--- drivers/serial/stm-asc.c
+++ drivers/serial/stm-asc.c
@@ -1,9 +1,10 @@
 /*
- * drivers/stm-asc.c
+ * drivers/serial/stm-asc.c
  *
  * Support for Serial I/O using STMicroelectronics' on-chip ASC.
  *
- *  Copyright (c) 2004  STMicroelectronics Limited
+ *  Copyright (c) 2004,2008  STMicroelectronics Limited
+ *  Sean McGoogan <Sean.McGoogan@st.com>
  *  Copyright (C) 1999  Takeshi Yaegachi & Niibe Yutaka
  *
  * This file is subject to the terms and conditions of the GNU Lesser
@@ -20,21 +21,20 @@
 #include "asm/io.h"
 #include "asm/pio.h"
 
-#define CREAD	0000200
-#define HUPCL	0002000
-#define CLOCAL	0004000
-#define	CSTOPB	0000100
-#define CSIZE	0000060
-#define CS7	0000040
-#define CS8	0000060
-#define PARENB	0000400
-#define PARODD  0001000
+#define CS7		0000040
+#define CS8		0000060
+#define CSIZE		0000060
+#define CSTOPB		0000100
+#define CREAD		0000200
+#define PARENB		0000400
+#define PARODD		0001000
+#define HUPCL		0002000
+#define CLOCAL		0004000
 
 #define BAUDMODE	0x00001000
 #define CTSENABLE	0x00000800
 #define RXENABLE	0x00000100
 #define RUN		0x00000080
-#define LOOPBACK	0x00000000
 #define STOPBIT		0x00000008
 #define MODE		0x00000001
 #define MODE_7BIT_PAR	0x0003
@@ -56,43 +56,42 @@
 #define STA_RBF		0x0001
 
 
-#define UART_BAUDRATE_OFFSET    0x00
-#define UART_TXBUFFER_OFFSET    0x04
-#define UART_RXBUFFER_OFFSET    0x08
-#define UART_CONTROL_OFFSET     0x0C
-#define UART_INTENABLE_OFFSET   0x10
-#define UART_STATUS_OFFSET      0x14
-#define UART_GUARDTIME_OFFSET   0x18
-#define UART_TIMEOUT_OFFSET     0x1C
-#define UART_TXRESET_OFFSET     0x20
-#define UART_RXRESET_OFFSET     0x24
-#define UART_RETRIES_OFFSET     0x28
+#define UART_BAUDRATE_OFFSET	0x00
+#define UART_TXBUFFER_OFFSET	0x04
+#define UART_RXBUFFER_OFFSET	0x08
+#define UART_CONTROL_OFFSET	0x0C
+#define UART_INTENABLE_OFFSET	0x10
+#define UART_STATUS_OFFSET	0x14
+#define UART_GUARDTIME_OFFSET	0x18
+#define UART_TIMEOUT_OFFSET	0x1C
+#define UART_TXRESET_OFFSET	0x20
+#define UART_RXRESET_OFFSET	0x24
+#define UART_RETRIES_OFFSET	0x28
 
-#define UART_BAUDRATE_REG   (CFG_STM_ASC_BASE + UART_BAUDRATE_OFFSET)
-#define UART_TXBUFFER_REG   (CFG_STM_ASC_BASE + UART_TXBUFFER_OFFSET)
-#define UART_RXBUFFER_REG   (CFG_STM_ASC_BASE + UART_RXBUFFER_OFFSET)
-#define UART_CONTROL_REG    (CFG_STM_ASC_BASE + UART_CONTROL_OFFSET)
-#define UART_INTENABLE_REG  (CFG_STM_ASC_BASE + UART_INTENABLE_OFFSET)
-#define UART_STATUS_REG     (CFG_STM_ASC_BASE + UART_STATUS_OFFSET)
-#define UART_GUARDTIME_REG  (CFG_STM_ASC_BASE + UART_GUARDTIME_OFFSET)
-#define UART_TIMEOUT_REG    (CFG_STM_ASC_BASE + UART_TIMEOUT_OFFSET)
-#define UART_TXRESET_REG    (CFG_STM_ASC_BASE + UART_TXRESET_OFFSET)
-#define UART_RXRESET_REG    (CFG_STM_ASC_BASE + UART_RXRESET_OFFSET)
-#define UART_RETRIES_REG    (CFG_STM_ASC_BASE + UART_RETRIES_OFFSET)
+#define UART_BAUDRATE_REG	(CFG_STM_ASC_BASE + UART_BAUDRATE_OFFSET)
+#define UART_TXBUFFER_REG	(CFG_STM_ASC_BASE + UART_TXBUFFER_OFFSET)
+#define UART_RXBUFFER_REG	(CFG_STM_ASC_BASE + UART_RXBUFFER_OFFSET)
+#define UART_CONTROL_REG	(CFG_STM_ASC_BASE + UART_CONTROL_OFFSET)
+#define UART_INTENABLE_REG	(CFG_STM_ASC_BASE + UART_INTENABLE_OFFSET)
+#define UART_STATUS_REG		(CFG_STM_ASC_BASE + UART_STATUS_OFFSET)
+#define UART_GUARDTIME_REG	(CFG_STM_ASC_BASE + UART_GUARDTIME_OFFSET)
+#define UART_TIMEOUT_REG	(CFG_STM_ASC_BASE + UART_TIMEOUT_OFFSET)
+#define UART_TXRESET_REG	(CFG_STM_ASC_BASE + UART_TXRESET_OFFSET)
+#define UART_RXRESET_REG	(CFG_STM_ASC_BASE + UART_RXRESET_OFFSET)
+#define UART_RETRIES_REG	(CFG_STM_ASC_BASE + UART_RETRIES_OFFSET)
 
 
 /*---- Values for the BAUDRATE Register -----------------------*/
 
-
-#if defined(CONFIG_SH_STB7100) || defined(CONFIG_SH_STX7111) || defined(CONFIG_SH_STX7200)
+#if defined(__SH4__)
 #define PCLK			(gd->bd->bi_emifrq*1000000)
 #define BAUDRATE_VAL_M0(bps)	(PCLK / (16 * (bps)))
-#define BAUDRATE_VAL_M1(bps)    ( (((bps * (1 << 14))+ (1<<13)) / (PCLK/(1 << 6))))
-#else
+#define BAUDRATE_VAL_M1(bps)	((((bps * (1 << 14))+ (1<<13)) / (PCLK/(1 << 6))))
+#else	/* !defined(__SH4__) */
 #define PCLK			B_CLOCK_RATE
 #define BAUDRATE_VAL_M0(bps)	(PCLK / (16 * (bps)))
 #define BAUDRATE_VAL_M1(bps)	(int)((((double)bps * (1 << 20))/ PCLK)+0.5)
-#endif
+#endif	/* defined(__SH4__) */
 
 /*
  * MODE 0
@@ -104,172 +103,185 @@
  *                   baudrate * 16 * 2^16
  * ASCBaudRate =   ------------------------
  *                          ICCLK
+ *
+ * NOTE:
+ * Mode 1 should be used for baudrates of 19200, and above, as it
+ * has a lower deviation error than Mode 0 for higher frequencies.
+ * Mode 0 should be used for all baudrates below 19200.
  */
-
-#define BPS_2400	BAUDRATE_VAL_M0(2400)
-#define BPS_4800	BAUDRATE_VAL_M0(4800)
-#define BPS_9600	BAUDRATE_VAL_M0(9600)
-#define BPS_19200	BAUDRATE_VAL_M1(19200)
-#define BPS_38400	BAUDRATE_VAL_M1(38400)
-#define BPS_57600	BAUDRATE_VAL_M1(57600)
-#define BPS_96000	BAUDRATE_VAL_M1(96000)
-#define BPS_115200	BAUDRATE_VAL_M1(115200)
 
 
 #if defined(CONFIG_STM_ST231)
-
 #define p2_outl(addr,b) writel(b,addr)
 #define p2_inl(addr)    readl(addr)
-
 #endif
 
 
-/* This function should be implemented for asc driver */
-void handleError (void)
+#ifdef CONFIG_HWFLOW
+static int hwflow = 0;		/* turned off by default */
+#endif	/* CONFIG_HWFLOW */
+
+
+/* busy wait until it is safe to send a char */
+static inline void TxCharReady (void)
 {
-	return;
+	unsigned long status;
+
+	do {
+		status = p2_inl (UART_STATUS_REG);
+	} while (status & STA_TF);
 }
 
-static int hwflow = 0;		/* turned off by default */
-
-int serial_init (void)
+/* initialize the ASC */
+extern int serial_init (void)
 {
 	DECLARE_GLOBAL_DATA_PTR;
-	int cflag = CREAD | HUPCL | CLOCAL | CSTOPB;
-	unsigned int status;
+	const int cflag = CREAD | HUPCL | CLOCAL | CSTOPB | CS8 | PARODD;
+	unsigned long val;
 	int baud = gd->baudrate;
-	int t;
-	int p2_val;
+	int t, mode=1;
+
 	switch (baud) {
+#if 0
 	case 0:
 		t = -1;
 		break;
 	case 2400:
-		t = BPS_2400;
+		t = BAUDRATE_VAL_M0(2400);
+		mode = 0;
 		break;
 	case 4800:
-		t = BPS_4800;
+		t = BAUDRATE_VAL_M0(4800);
+		mode = 0;
 		break;
+#endif
 	case 9600:
-		t = BPS_9600;
+		t = BAUDRATE_VAL_M0(9600);
+		mode = 0;
 		break;
 	case 19200:
-		t = BPS_19200;
+		t = BAUDRATE_VAL_M1(19200);
 		break;
 	case 38400:
-		t = BPS_38400;
+		t = BAUDRATE_VAL_M1(38400);
 		break;
 	case 57600:
-		t = BPS_57600;
+		t = BAUDRATE_VAL_M1(57600);
 		break;
 	default:
-		printf ("sci: unsupported baud rate: %d, using 115200 instead.\n", baud);
+		printf ("ASC: unsupported baud rate: %d, using 115200 instead.\n", baud);
 	case 115200:
-		t = BPS_115200;
+		t = BAUDRATE_VAL_M1(115200);
 		break;
 	}
 
-	cflag |= CS8;
-	cflag |= PARODD;
-
 	/* wait for end of current transmission */
-	do {
-		status = p2_inl (UART_STATUS_REG);
-	} while (status & STA_TF);
+	TxCharReady ();
 
-	p2_val = p2_inl (UART_CONTROL_REG);
-	p2_outl (UART_CONTROL_REG, (p2_val & ~RUN));
-	p2_val = RXENABLE;
+	/* disable the baudrate generator */
+	val = p2_inl (UART_CONTROL_REG);
+	p2_outl (UART_CONTROL_REG, (val & ~RUN));
 
-	/* reset fifo rx e tx */
+	/* set baud generator reload value */
+	p2_outl (UART_BAUDRATE_REG, t);
+
+	/* reset the RX & TX buffers */
 	p2_outl (UART_TXRESET_REG, 1);
 	p2_outl (UART_RXRESET_REG, 1);
 
+	/* build up the value to be written to CONTROL */
+	val = RXENABLE | RUN;
+
 	/* set character length */
 	if ((cflag & CSIZE) == CS7)
-		p2_val |= MODE_7BIT_PAR;
+		val |= MODE_7BIT_PAR;
 	else {
 		if (cflag & PARENB)
-			p2_val |= MODE_8BIT_PAR;
+			val |= MODE_8BIT_PAR;
 		else
-			p2_val |= MODE_8BIT;
+			val |= MODE_8BIT;
 	}
 
 	/* set stop bit */
 	/* it seems no '0 stop bits' option is available: by default
 	 * we get 0.5 stop bits */
 	if (cflag & CSTOPB)
-		p2_val |= STOP_1BIT;
+		val |= STOP_1BIT;
 
 	/* odd parity */
 	if (cflag & PARODD)
-		p2_val |= PARITYODD;
+		val |= PARITYODD;
 
+#ifdef CONFIG_HWFLOW
 	/*  set flow control */
 	if (hwflow)
-		p2_val |= CTSENABLE;
+		val |= CTSENABLE;
+#endif	/* CONFIG_HWFLOW */
 
-	/* set speed and baud generator mode */
-	p2_outl (UART_BAUDRATE_REG, t);
-	p2_val |= BAUDMODE;
+	/* set baud generator mode */
+	if (mode)
+		val |= BAUDMODE;
 
-	/* write final value and enable port */
-	p2_outl (UART_CONTROL_REG, (p2_val | RUN));
+	/* finally, write value and enable ASC */
+	p2_outl (UART_CONTROL_REG, val);
 	return 0;
 }
 
-int serial_tstc (void)
+/* returns TRUE if a char is available, ready to be read */
+extern int serial_tstc (void)
 {
-	unsigned short status;
+	unsigned long status;
 
 	status = p2_inl (UART_STATUS_REG);
 	return (status & STA_RBF);
 }
 
-/* This function doesn't correctly work */
-int serial_getc (void)
+/* blocking function, that returns next char */
+extern int serial_getc (void)
 {
 	char ch;
 
+	/* polling wait: for a char to be read */
 	while (!serial_tstc ());
 
+	/* read char, now that we know we have one */
 	ch = p2_inl (UART_RXBUFFER_REG);
 
+	/* return consumed char to the caller */
 	return ch;
 }
 
-static inline int putDebugCharReady (void)
+/* write write out a single char */
+extern void serial_putc (char ch)
 {
-	unsigned long status;
-
-	status = p2_inl (UART_STATUS_REG);
-	return !(status & STA_TF);
-}
-
-void serial_putc (char ch)
-{
+	/* Stream-LF to CR+LF conversion */
 	if (ch == 10)
 		serial_putc ('\r');
-	while (!putDebugCharReady ());
+
+	/* wait till safe to write next char */
+	TxCharReady ();
+
+	/* finally, write next char */
 	p2_outl (UART_TXBUFFER_REG, ch);
 }
 
-void serial_puts (const char *s)
+/* write an entire (NUL-terminated) string */
+extern void serial_puts (const char *s)
 {
 	while (*s) {
 		serial_putc (*s++);
 	}
 }
 
-
-void serial_setbrg (void)
+/* called to adjust baud-rate */
+extern void serial_setbrg (void)
 {
-
-  serial_init();
+	/* just re-initialize ASC */
+	serial_init ();
 }
 
 #ifdef CONFIG_HWFLOW
-int hwflow_onoff (int on)
+extern int hwflow_onoff (int on)
 {
 	switch (on) {
 	case 0:
@@ -286,6 +298,6 @@ int hwflow_onoff (int on)
 	}
 	return hwflow;
 }
-#endif
+#endif	/* CONFIG_HWFLOW */
 
 #endif	/* CONFIG_STM_ASC_SERIAL */
