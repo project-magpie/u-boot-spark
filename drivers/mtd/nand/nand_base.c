c524400bc1d0bcf4c33a8ac5c5409f906b8ff1f1
Karl Beldan <karl.beldan@gmail.com>
2008-09-15 16:08:03 +0200

    NAND: Reset chip on power-up
    Some chips require a RESET after power-up (e.g. Micron MT29FxGxxxxx).
    The first command sent is NAND_CMD_READID.
    Issue a NAND_CMD_RESET in nand_scan_ident before reading the device id.
    Tested with an MT29F4G08AAC.
    
    See http://git.bri.st.com/?p=Mirrors/u-boot.git;a=commit;h=33efde5ecac91ab118ff00b95a181fd6d75f8645
    
    Signed-off-by: Karl Beldan <karl.beldan@gmail.com>
    Signed-off-by: Scott Wood <scottwood@freescale.com>
    Signed-off-by: Sean McGoogan <Sean.McGoogan@st.com>

--- drivers/mtd/nand/nand_base.c
+++ drivers/mtd/nand/nand_base.c
@@ -2319,6 +2319,12 @@ int nand_scan (struct mtd_info *mtd, int maxchips)
 	/* Select the device */
 	this->select_chip(mtd, 0);
 
+	/*
+	 * Reset the chip, required by some chips (e.g. Micron MT29FxGxxxxx)
+	 * after power-up
+	 */
+	this->cmdfunc(mtd, NAND_CMD_RESET, -1, -1);
+
 	/* Send the command for reading device ID */
 	this->cmdfunc (mtd, NAND_CMD_READID, 0x00, -1);
 
@@ -2425,6 +2431,9 @@ int nand_scan (struct mtd_info *mtd, int maxchips)
 
 	for (i=1; i < maxchips; i++) {
 		this->select_chip(mtd, i);
+
+		/* See comment in nand_get_flash_type for reset */
+		this->cmdfunc(mtd, NAND_CMD_RESET, -1, -1);
 
 		/* Send the command for reading device ID */
 		this->cmdfunc (mtd, NAND_CMD_READID, 0x00, -1);
