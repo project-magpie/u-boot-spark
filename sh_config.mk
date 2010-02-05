63b8c361708a40185a438dd882b83ad649a4ed3a
Sean McGoogan <Sean.McGoogan@st.com>
2010-02-05 18:31:01 +0000

    [SH] Allow boot-from-SPI on the FLI7510 Development Board (FLDB)
    This update provides working support for STMicroelectronics' FLI7510
    (Freeman Lite) Development Board (FLDB_GPD201), by slowing down the SPI
    clock further, to allow boot-from-SPI to boot successfully.
    
    The SPI controller has a register which controls how much to divide
    the clock by. For most SoCs + boards, setting this register to 4 is
    sufficient.  However, for the FLI7510 Development Board (FLDB), the
    divider register needs to be increased to a value of 6.
    
    This update will convert:
    
    	*ST40_EMI_SPI_CLOCKDIV = 4
    
    to
    
    	*ST40_EMI_SPI_CLOCKDIV = 6
    
    for the FLI7510.
    
    All testing was just done on one board FLDB_GPD201 (S/N: SZ015).
    
    Signed-off-by: Sean McGoogan <Sean.McGoogan@st.com>

--- sh_config.mk
+++ sh_config.mk
@@ -1,5 +1,5 @@
 #
-# (C) Copyright 2004-2009
+# (C) Copyright 2004-2010
 # Andy Sturges, STMicroelectronics, andy.sturges@st.com
 # Sean McGoogan STMicroelectronics, <Sean.McGoogan@st.com>
 #
@@ -27,7 +27,7 @@
 # for actual build number, in the following identifier.
 # Regular Expression for string is:
 # 	"stm[2-9][0-9]-[2-9][0-9][0-9][0-9]-[01][0-9]-[0-3][0-9]"
-SH_IDENT_STRING="\"stm23-2009-12-07\""
+SH_IDENT_STRING="\"stm23-2010-02-05\""
 
 PLATFORM_CPPFLAGS += -DCONFIG_SH4 -D__SH4__ -DCONFIG_IDENT_STRING=$(SH_IDENT_STRING)
 PLATFORM_LDFLAGS  += -n
