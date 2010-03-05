c7e1c5b2e68857aeb761e906519a214c07af272f
Sean McGoogan <Sean.McGoogan@st.com>
2010-03-05 18:38:13 +0000

    [SH] First pass support for STMicroelectronics' EUD7141 Board
    This is a first pass (snapshot) update to support the EUD7141 board,
    which uses ST's STx7141 SoC device.
    
    This version provides support for RS-232 console, NOR-flash, NAND-flash,
    boot-from-NAND, and for both 29-bit and 32-bit modes.
    
    Minimal testing has been done, on only one board: STi7141-EUD_MB_V1.0A-012
    
    Signed-off-by: Sean McGoogan <Sean.McGoogan@st.com>

---
+++ board/st/eud7141/eud7141.c
@@ -0,0 +1,88 @@
+/*
+ * (C) Copyright 2008-2010 STMicroelectronics.
+ *
+ * Sean McGoogan <Sean.McGoogan@st.com>
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <common.h>
+#include <command.h>
+#include <asm/soc.h>
+#include <asm/stx7141reg.h>
+#include <asm/io.h>
+#include <asm/pio.h>
+
+
+extern void flashWriteEnable(void)
+{
+	/* Enable Vpp for writing to flash */
+	/* Nothing to do! */
+}
+
+extern void flashWriteDisable(void)
+{
+	/* Disable Vpp for writing to flash */
+	/* Nothing to do! */
+}
+
+static void configPIO(void)
+{
+	unsigned long sysconf;
+
+	/* Setup PIO of ASC device */
+	SET_PIO_ASC(PIO_PORT(1), 0, 1, STPIO_NO_PIN, STPIO_NO_PIN);  /* ASC2 */
+
+	/* Enable ASC UART */
+	sysconf = *STX7141_SYSCONF_SYS_CFG36;
+	/* CFG36[31] = 0 = UART2_RXD_SEL = PIO1[1] */
+	sysconf &= ~(1ul << 31);
+	*STX7141_SYSCONF_SYS_CFG36 = sysconf;
+
+	/* Route ASC2 via PIO[1] for TX & RX */
+	sysconf = *STX7141_SYSCONF_SYS_CFG19;
+	/* PIO1[0] Selector: CFG19[1:0] = 3 TX */
+	/* PIO1[1] Selector: CFG19[3:2] = 3 RX */
+	sysconf |= 3ul << 0 | 3ul << 2;
+	*STX7141_SYSCONF_SYS_CFG19 = sysconf;
+}
+
+extern int board_init(void)
+{
+	configPIO();
+
+#if defined(CONFIG_SH_STM_SATA)
+	stx7141_configure_sata ();
+#endif	/* CONFIG_SH_STM_SATA */
+
+	return 0;
+}
+
+extern int checkboard (void)
+{
+	printf ("\n\nBoard: STx7141-EUD"
+#ifdef CONFIG_SH_SE_MODE
+		"  [32-bit mode]"
+#else
+		"  [29-bit mode]"
+#endif
+		"\n");
+
+	return 0;
+}
