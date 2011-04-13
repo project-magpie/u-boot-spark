8e36c76c177ef02c429066322b4f04ea16552da4
Vipul Samar <vipulkumar.samar@st.com>
2011-04-13 19:30:15 +0530

    SPEAr1340:workaround to add mach type for spear1340
    This patch adds MACH type of SPEAr1340 in arch/arm/include/asm/mach-types.h
    file. Revert this patch after ARM update mach-type.
    
    Signed-off-by: Vipul Samar <vipulkumar.samar@st.com>

--- arch/arm/include/asm/mach-types.h
+++ arch/arm/include/asm/mach-types.h
@@ -2863,6 +2863,7 @@ extern unsigned int __machine_arch_type;
 #define MACH_TYPE_TINY_GURNARD         2880
 #define MACH_TYPE_SPEAR1310            2881
 #define MACH_TYPE_SPEAR900             3061
+#define MACH_TYPE_SPEAR1340            3394
 
 #ifdef CONFIG_ARCH_EBSA110
 # ifdef machine_arch_type
@@ -37076,6 +37077,17 @@ extern unsigned int __machine_arch_type;
 # define machine_is_spear900()	(0)
 #endif
 
+#ifdef CONFIG_MACH_SPEAR1340
+# ifdef machine_arch_type
+#  undef machine_arch_type
+#  define machine_arch_type	__machine_arch_type
+# else
+#  define machine_arch_type	MACH_TYPE_SPEAR1340
+# endif
+# define machine_is_spear1340()	(machine_arch_type == MACH_TYPE_SPEAR1340)
+#else
+# define machine_is_spear1340()	(0)
+#endif
 
 /*
  * These have not yet been registered
