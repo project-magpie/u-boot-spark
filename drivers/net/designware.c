7de3ba6c98ef1c6e201aa635364a4bf2ee0304d3
Vipin Kumar <vipin.kumar@st.com>
2011-06-28 15:06:58 +0530

    dw_eth: Add interface information to ethernet driver initialization
    Few Designware peripheral registers need to be modified based on the ethernet
    interface selected by the board. This patch supports interface information in
    ethernet driver
    
    Signed-off-by: Vipin Kumar <vipin.kumar@st.com>

--- drivers/net/designware.c
+++ drivers/net/designware.c
@@ -162,7 +162,13 @@ static int dw_eth_init(struct eth_device *dev, bd_t *bis)
 
 	conf = FRAMEBURSTENABLE | DISABLERXOWN;
 
-	if (priv->speed != SPEED_1000M)
+	if (priv->speed == SPEED_100M) {
+		conf |= MII_PORTSELECT;
+
+		if ((priv->interface == PHY_INTERFACE_MODE_RMII) ||
+			(priv->interface == PHY_INTERFACE_MODE_RGMII))
+			conf |= FES_100;
+	} else if (priv->speed == SPEED_10M)
 		conf |= MII_PORTSELECT;
 
 	if (priv->duplex == FULL_DUPLEX)
@@ -483,7 +489,7 @@ static int dw_mii_write(char *devname, u8 addr, u8 reg, u16 val)
 }
 #endif
 
-int designware_initialize(u32 id, ulong base_addr, u32 phy_addr)
+int designware_initialize(u32 id, ulong base_addr, u32 phy_addr, u32 interface)
 {
 	struct eth_device *dev;
 	struct dw_eth_dev *priv;
@@ -516,6 +522,7 @@ int designware_initialize(u32 id, ulong base_addr, u32 phy_addr)
 	priv->dma_regs_p = (struct eth_dma_regs *)(base_addr +
 			DW_DMA_BASE_OFFSET);
 	priv->address = phy_addr;
+	priv->interface = interface;
 
 	if (mac_reset(dev) < 0)
 		return -1;
