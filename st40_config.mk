1a44831f28d4deead7dd685943ddabcdecd1e116
Sean McGoogan <Sean.McGoogan@st.com>
2011-07-07 17:37:43 +0100

    [ST40] Complete Merge of 'v1.3.2' into STMicroelectronics' ST40 tree
    This commit completes the merge of the official 'v1.3.2' release (which
    includes Renesas SuperH sh3 and sh4 trees) and the STMicroelectronics'
    ST40 tree. The two trees are in different name spaces, and hence should
    not occlude or conflict with each other.
    
    Specifically, *all* the delinquencies are now (believed to be) addressed,
    and there are no (known) outstanding issues with the merge to 'v1.3.2'.
    
    see commit 56c8196349ed7b8cc54baabea31b359e462216d2
    
    Signed-off-by: Sean McGoogan <Sean.McGoogan@st.com>

--- st40_config.mk
+++ st40_config.mk
@@ -27,7 +27,7 @@
 # for actual build number, in the following identifier.
 # Regular Expression for string is:
 # 	"stm[2-9][0-9]-[2-9][0-9][0-9][0-9]-[01][0-9]-[0-3][0-9]"
-ST40_IDENT_STRING="\"stm24-2011-07-06\""
+ST40_IDENT_STRING="\"stm24-2011-07-07\""
 
 PLATFORM_CPPFLAGS += -DCONFIG_ST40 -D__SH4__ -DCONFIG_IDENT_STRING=$(ST40_IDENT_STRING)
 PLATFORM_LDFLAGS  += -n
