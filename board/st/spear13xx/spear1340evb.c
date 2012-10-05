98001a8c6725aa29944758b8c5498e16e09546d3
Vinit Shenoy <vinit.shenoy@st.com>
2012-10-05 11:04:26 +0530

    SPEAr1340 evb padmux: Disable SSP chipselects cs2 and cs3
    On SPEAr 1340 Rev-1.0 boards, ssp chipselect pins cs-2 and cs-3 are used
    for DDR-CKE and DDR-RST, so enabling these pins causes U-boot
    initialization to fail.
    
    On board revisions Rev-2.1 and above the same pins are used for DDR as
    well, but resistors R127 and R128 are not loaded, so no issues occur.
    
    Disable these pins, so that U-Boot can be used for Rev-1.0 boards as
    well.
    
    Signed-off-by: Vinit Shenoy <vinit.shenoy@st.com>
    Reviewed-by: Vipin Kumar <vipin.kumar@st.com>

--- board/st/spear13xx/spear1340evb.c
+++ board/st/spear13xx/spear1340evb.c
@@ -44,8 +44,6 @@ int board_early_init_f(void)
 	spear1340_enable_pins(PMX_I2C0, 0);
 	spear1340_enable_pins(PMX_SSP0, PMX_SSP_CS0);
 	spear1340_enable_pins(PMX_SSP0, PMX_SSP_CS1);
-	spear1340_enable_pins(PMX_SSP0, PMX_SSP_CS2);
-	spear1340_enable_pins(PMX_SSP0, PMX_SSP_CS3);
 	spear1340_enable_pins(PMX_SMI, 0);
 	spear1340_enable_pins(PMX_SDMMC, 0);
 	spear1340_enable_pins(PMX_ETH0, PMX_ETH_RGMII);
