1883be3d76464497eee8ac3c3ba390e415edfc5c
Sean McGoogan <Sean.McGoogan@st.com>
2013-02-18 15:39:58 +0000

    [ST40] Check parameters when using the RET_xxx() re-timing macros.
    This commit forces all the RET_xxx() re-timing macros, which are used to
    initialize an instance of the "stm_pio_control_retime_config" structure,
    to use "valid" values only. Specifically, for the "clk" and the "delay"
    (multi-bit) bitfields.
    
    If an attempt is made to pass in invalid parameter to the RET_xxx()
    macros, then a compilation error will now result. This is particularly
    useful now that the sizes of the above two bitfields (in H/W) can vary
    depending on the actual SoC being used. Also, the semantics of the
    "delay" bitfield in the linux kernel sources are now in units of ps
    (pico-seconds), but in U-Boot it is just the raw binary value to be
    programmed. This should avoid users unwittingly copying code from linux,
    to configure the re-timings registers, and the compiler silently ignoring
    the paradigm shift (in semantics) in U-Boot.
    
    This has not been tested, apart from compilation.
    
    Signed-off-by: Sean McGoogan <Sean.McGoogan@st.com>

--- include/asm-st40/pio-control.h
+++ include/asm-st40/pio-control.h
@@ -23,6 +23,42 @@ struct stm_pio_control_retime_config {
 	int delay:5;		/* was previously "delay_input:3" */
 };
 
+
+/*
+ * list of valid values for the "clk" bitfield above
+ */
+#define STM_RETIME_VALUE_CLK_0			(0)
+#define STM_RETIME_VALUE_CLK_1			(1)
+	/* if the H/W bitfield is 2-bits wide ? */
+#if defined(CONFIG_ST40_STXH416)
+//#define STM_RETIME_VALUE_CLK_2		(2)
+//#define STM_RETIME_VALUE_CLK_3		(3)
+#endif	/* H/W bitfield is 2-bits wide */
+
+/*
+ * list of valid values for the "delay" bitfield above
+ */
+#define STM_RETIME_VALUE_DELAY_0		(0)
+#define STM_RETIME_VALUE_DELAY_1		(1)
+#define STM_RETIME_VALUE_DELAY_2		(2)
+#define STM_RETIME_VALUE_DELAY_3		(3)
+	/* if the H/W bitfield is 4-bits wide ? */
+#if defined(CONFIG_ST40_STXH416)
+//#define STM_RETIME_VALUE_DELAY_4		(4)
+//#define STM_RETIME_VALUE_DELAY_5		(5)
+//#define STM_RETIME_VALUE_DELAY_6		(6)
+//#define STM_RETIME_VALUE_DELAY_7		(7)
+//#define STM_RETIME_VALUE_DELAY_8		(8)
+//#define STM_RETIME_VALUE_DELAY_9		(9)
+//#define STM_RETIME_VALUE_DELAY_10		(10)
+//#define STM_RETIME_VALUE_DELAY_11		(11)
+//#define STM_RETIME_VALUE_DELAY_12		(12)
+//#define STM_RETIME_VALUE_DELAY_13		(13)
+//#define STM_RETIME_VALUE_DELAY_14		(14)
+//#define STM_RETIME_VALUE_DELAY_15		(15)
+#endif	/* H/W bitfield is 4-bits wide */
+
+
 /* 	Generic Retime Padlogic possible modes
  * Refer to GRP Functional specs (ADCS 8198257) for more details */
 
@@ -30,66 +66,66 @@ struct stm_pio_control_retime_config {
  * Bypass retime with optional delay */
 #define	RET_BYPASS(_delay) (&(struct stm_pio_control_retime_config){ \
 		.retime = 0, \
-		.clk = 0, \
+		.clk = STM_RETIME_VALUE_CLK_0, \
 		.clknotdata = 0, \
 		.double_edge = 0, \
 		.invertclk = 0, \
-		.delay = _delay, \
+		.delay = STM_RETIME_VALUE_DELAY_ ## _delay, \
 		})
 /*
  * (R0, R1, R0D, R1D modes )
  * single-edge data non inverted clock, retime data with clk */
 #define	RET_SE_NICLK_IO(_delay, _clk) (&(struct stm_pio_control_retime_config){ \
 		.retime = 1, \
-		.clk = _clk, \
+		.clk = STM_RETIME_VALUE_CLK_ ## _clk, \
 		.clknotdata = 0, \
 		.double_edge = 0, \
 		.invertclk = 0, \
-		.delay = _delay, \
+		.delay = STM_RETIME_VALUE_DELAY_ ## _delay, \
 		})
 
 /* RIV0, RIV1, RIV0D, RIV1D modes
  * single-edge data inverted clock, retime data with clk */
 #define	RET_SE_ICLK_IO(_delay, _clk) (&(struct stm_pio_control_retime_config){ \
 		.retime = 1, \
-		.clk = _clk, \
+		.clk = STM_RETIME_VALUE_CLK_ ## _clk, \
 		.clknotdata = 0, \
 		.double_edge = 0, \
 		.invertclk = 1, \
-		.delay = _delay, \
+		.delay = STM_RETIME_VALUE_DELAY_ ## _delay, \
 		})
 
 /* R0E, R1E, R0ED, R1ED modes
  * double-edge data, retime data with clk */
 #define	RET_DE_IO(_delay, _clk) (&(struct stm_pio_control_retime_config){ \
 		.retime = 1, \
-		.clk = _clk, \
+		.clk = STM_RETIME_VALUE_CLK_ ## _clk, \
 		.clknotdata = 0, \
 		.double_edge = 1, \
 		.invertclk = 0, \
-		.delay = _delay, \
+		.delay = STM_RETIME_VALUE_DELAY_ ## _delay, \
 		})
 
 /* CIV0, CIV1 modes with inverted clock
  * Retiming the clk pins will park clock & reduce the noise within the core. */
 #define RET_ICLK(_clk) (&(struct stm_pio_control_retime_config){ \
 		.retime = 1, \
-		.clk = _clk, \
+		.clk = STM_RETIME_VALUE_CLK_ ## _clk, \
 		.clknotdata = 1, \
 		.double_edge = 0, \
 		.invertclk = 1, \
-		.delay = 0, \
+		.delay = STM_RETIME_VALUE_DELAY_0, \
 		})
 
 /* CLK0, CLK1 modes with non-inverted clock
  * Retiming the clk pins will park clock & reduce the noise within the core. */
 #define RET_NICLK(_clk) (&(struct stm_pio_control_retime_config){ \
 		.retime = 1, \
-		.clk = _clk, \
+		.clk = STM_RETIME_VALUE_CLK_ ## _clk, \
 		.clknotdata = 1, \
 		.double_edge = 0, \
 		.invertclk = 0, \
-		.delay = 0, \
+		.delay = STM_RETIME_VALUE_DELAY_0, \
 		})
 
 #endif	/* __ASM_ST40_PIO_CONTROL_H */
