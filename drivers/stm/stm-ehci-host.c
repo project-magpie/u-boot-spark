8f9057b0dd40e64d693fb2ef578f236c7efe5443
Sean McGoogan <Sean.McGoogan@st.com>
2013-12-12 20:10:06 +0000

    [STM,FIX] Always shutdown USB EHCI before booting linux
    Prior to this commit, and despite the name, the U-Boot command "usb
    stop" did not actually stop the EHCI (USB 2.x) controller! In contrast,
    "usb stop" did work correctly for OHCI (USB 1.x).
    
    Within the confines of U-Boot, this is not a significant problem, as
    U-Boot owns all the memory.  However, when chain-loading a subsequent
    "payload application" (e.g. an OS), then this can be a very significant
    problem indeed!  As a result, when linux was booted from U-Boot (after
    starting the EHCI controller), then the EHCI controller would continue to
    access (and modify) system RAM whilst linux was still initializing itself.
    This clearly can result in dangerous and undefined behaviour!
    
    This commit adds code to ensure that the EHCI controller is shutdown
    cleanly when "usb stop" is performed. This new code will also be
    *implicitly* called when "bootm" is used, if USB was started. Hence,
    there is no need to call "usb stop" manually.
    
    Tested on a single B2020 board with a STxH415 SoC, using the ST40 core.
    Linux was not booted, instead the state of the EHCI (and OHCI) USB
    controllers were dumped immediately prior to the point where control
    would normally be passed to linux, and it was confirmed that both USB
    controllers were not running.
    
    Please see the following for further information:
    
    	https://bugzilla.stlinux.com/show_bug.cgi?id=41055
    	https://bugzilla.stlinux.com/show_bug.cgi?id=41767
    
    Signed-off-by: Sean McGoogan <Sean.McGoogan@st.com>

--- drivers/stm/stm-ehci-host.c
+++ drivers/stm/stm-ehci-host.c
@@ -1,6 +1,6 @@
 /*
  * (C) Copyright 2008, Michael Trimarchi <trimarchimichael@yahoo.it>
- * Copyright (C) 2012-2013 STMicroelectronics Limited
+ * Copyright (C) 2012-2014 STMicroelectronics Limited
  *	Sean McGoogan <Sean.McGoogan@st.com>
  *
  * Author: Michael Trimarchi <trimarchimichael@yahoo.it>
@@ -30,6 +30,8 @@
 
 #define unlikely(x)		__builtin_expect(!!(x), 0)
 
+#define STM_HC_LENGTH		0x10	/* should always be 16 for ST's SoCs */
+
 
 /*
  * Create the appropriate control structures to manage
@@ -58,9 +60,9 @@ extern int ehci_hcd_init(
 	*hccr = (struct ehci_hccr *)AHB2STBUS_EHCI_BASE;
 
 	hc_length = HC_LENGTH(ehci_readl(&(*hccr)->cr_capbase));
-	BUG_ON(hc_length != 0x10);	/* should always be 16 for ST's SoCs */
+	BUG_ON(hc_length != STM_HC_LENGTH);
 
-	*hcor = (struct ehci_hcor *)((uint32_t)*hccr + hc_length);
+	*hcor = (struct ehci_hcor *)((uint32_t)*hccr + STM_HC_LENGTH);
 
 #if 1
 	printf("STM EHCI HCD initialization HCCR=0x%08x, HCOR=0x%08x, hc_length=%u\n",
@@ -72,10 +74,54 @@ extern int ehci_hcd_init(
 
 
 /*
- * Destroy the appropriate control structures corresponding
- * to the EHCI host controller.
+ * Properly shutdown the EHCI host controller (HC).
+ * This is a mandatory requirement before we can safely transition
+ * from U-Boot to any OS (e.g. linux) … otherwise madness ensues!
  */
 extern int ehci_hcd_stop(const int index)
 {
+	struct ehci_hccr * const hccr =
+		(struct ehci_hccr *)AHB2STBUS_EHCI_BASE;
+	struct ehci_hcor * const hcor =
+		(struct ehci_hcor *)((uint32_t)hccr + STM_HC_LENGTH);
+	uint32_t reg;
+	const int numPorts = HCS_N_PORTS(ehci_readl(&hccr->cr_hcsparams));
+	int port;
+
+	/*
+	 * Complete any current transactions, and HALT the HC.
+	 */
+	reg = ehci_readl(&hcor->or_usbcmd);
+	reg &= ~(CMD_RUN);
+	ehci_writel(&hcor->or_usbcmd, reg);
+
+	/*
+	 * Wait for the HC to be halted.
+	 * This can take up to 16 micro-frames (i.e. 2ms).
+	 */
+	while (!(ehci_readl(&hcor->or_usbsts) & STS_HALT))
+		;	/* wait until STS_HALT is asserted */
+
+	/*
+	 * power-down all the root ports.
+	 */
+	for (port=0; port<numPorts; port++) {
+		ehci_writel(&hcor->or_portsc[port],
+			EHCI_PS_OCC | EHCI_PS_PEC | EHCI_PS_CSC);
+	}
+
+	/*
+	 * Finally, reset the controller (just in case)!
+	 */
+	reg = ehci_readl(&hcor->or_usbcmd);
+	reg |= CMD_RESET;
+	ehci_writel(&hcor->or_usbcmd, reg);
+
+	/*
+	 * … and wait for the reset to self-clear…
+	 */
+	while ((ehci_readl(&hcor->or_usbcmd) & CMD_RESET))
+		;	/* wait until CMD_RESET is de-asserted */
+
 	return 0;
 }
