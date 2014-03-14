22aa712345d3d9c65f9d61b7ff8a0e471e4b107c
Sachin Verma <sachin.verma@st.com>
2014-03-14 18:53:12 +0530

    [STM] Fix compiler warning for redefinition of NAND_MAX_OOBSIZE
    This commit fixes a compiler warning for redefinition of a macro
    defining the Maximum OOB size of NAND flashes.
    
    Signed-off-by: Sachin Verma <sachin.verma@st.com>

--- include/linux/mtd/nand.h
+++ include/linux/mtd/nand.h
@@ -47,7 +47,6 @@ extern void nand_wait_ready(struct mtd_info *mtd);
  * is supported now. If you add a chip with bigger oobsize/page
  * adjust this accordingly.
  */
- #define NAND_MAX_OOBSIZE	640
 #if 0	/* QQQ - moved to include/linux/mtd/mtd-abi.h */
 #define NAND_MAX_OOBSIZE	576
 #define NAND_MAX_PAGESIZE	8192
